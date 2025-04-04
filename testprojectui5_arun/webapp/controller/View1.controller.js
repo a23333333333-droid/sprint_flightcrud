701524149476a3152d091b051fdb07fa24eb7262
P Arun Kumar Reddy <p-arun-kumar.reddy@capgemini.com>
2025-04-04 12:01:33 +0000

    first git push

---
+++ testprojectui5_arun/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("testprojectui5arun.controller.View1", {
+        onInit() {
+        }
+    });
+});
